a39b6602fe2a7deeca1abae943efeb9eae4a64f1
Shruti Atitkar <118574196+shrutia898@users.noreply.github.com>
2025-04-06 17:08:30 -0400

    Update Art.java
    Getters and setters that can be linked to the save and canvasGUI

--- Art.java
+++ Art.java
@@ -1,2 +1,46 @@
 public class Art {
+    private String artName;
+    private String artTime;
+    private String artUser;
+    private String artLocation;
+    private String artDescription;
+    
+  public String getArtName() {
+    return artName;
+  }
+
+  public void setName(String artName) {
+    this.artName = artName;
+  }
+   public String getArTime() {
+    return artTime;
+  }
+
+  public void setArtTime(String artTime) {
+    this.artTime = artTime;
+  }
+  
+  public String getArtUser() {
+    return artUser;
+  }
+
+  public void setArtUser(String artUser) {
+    this.artUser = artUser;
+  }
+  
+  public String getArtLocation() {
+    return artLocation;
+  }
+
+  public void setArtLocation(String artLocation) {
+    this.artLocation = artLocation;
+  }
+  
+  public String getArtDescription() {
+    return artDescription;
+  }
+
+  public void setArtDescription(String artDescription) {
+    this.artDescription = artDescription;
+  }
 }
