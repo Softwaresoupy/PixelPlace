eab6b4837e0029fd766705bfd6987daaab34a424
ZiiYuuuu <caoziyu3495@gmail.com>
2025-04-25 15:27:50 -0400

    Create Artwork.java

---
+++ Artwork.java
@@ -0,0 +1,35 @@
+// TEST artwork class
+package artgallery;
+
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+
+public class Artwork {
+    private String title;
+    private User artist;
+    private Image thumbnail;
+
+    public Artwork(String title, User artist) {
+        this.title = title;
+        this.artist = artist;
+        this.thumbnail = getDefaultThumbnail();
+    }
+
+    private Image getDefaultThumbnail() {
+        return new ImageIcon("placeholder.jpg").getImage();
+    }
+
+    public Image getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(Image thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
