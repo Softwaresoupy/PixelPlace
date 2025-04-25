f32522cc2dd7190197840ec1b9eae81ea2ebfb50
ZiiYuuuu <caoziyu3495@gmail.com>
2025-04-25 15:29:10 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author caoziyu
- */
+package artgallery;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -14,10 +6,9 @@ import java.util.List;
 
 public class User implements Serializable {
     private String username;
-    private String password; // 实际项目中应加密存储
+    private String password;
     private String email;
-    private List<Art> uploadedArtworks;
-    //private List<Gallery> createdGalleries;
+    private List<Artwork> uploadedArtworks;
     private List<User> following;
 
     public User(String username, String password, String email) {
@@ -25,29 +16,18 @@ public class User implements Serializable {
         this.password = password;
         this.email = email;
         this.uploadedArtworks = new ArrayList<>();
-        //this.createdGalleries = new ArrayList<>();
         this.following = new ArrayList<>();
     }
 
-    // Getters and Setters
     public String getUsername() { return username; }
     public String getPassword() { return password; }
     public String getEmail() { return email; }
-    public List<Art> getUploadedArtworks() { return uploadedArtworks; }
-
-    public void setUploadedArtworks(List something) {uploadedArtworks = something;}
-    //public List<Gallery> getCreatedGalleries() { return createdGalleries; }
+    public List<Artwork> getUploadedArtworks() { return uploadedArtworks; }
     public List<User> getFollowing() { return following; }
-    public void setFollowing(List something) {following = something;}
 
-    public void uploadArtwork(Art artwork) {
+    public void uploadArtwork(Artwork artwork) {
         uploadedArtworks.add(artwork);
     }
-
-    /*public void createGallery(Gallery gallery) {
-        createdGalleries.add(gallery);
-    }
-     */
 
     public void followArtist(User artist) {
         if (!following.contains(artist)) {
@@ -67,10 +47,5 @@ public class User implements Serializable {
     public int hashCode() {
         return username.hashCode();
     }
-
-    public String toString(){
-        String userString = username + "'*%" + password + "'*%" + email + "'*%";
-        return userString;
-    }
-
 }
+
